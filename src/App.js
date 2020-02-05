041821e3922c1474dc6f00417e340247a4b80aad
Mark Goldhardt <markgoldhardt@Marks-iMac.hsd1.ut.comcast.net>
2020-02-04 22:11:14 -0700

    page posts loading

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
+import "./components/PostsContainer/PostsPage";
+import "./components/SearchBar/SearchBarContainer";
 // import dummydata
-
+import "./dummy-data"
+import {dummyData} from "./dummy-data";
+import {PostsPage} from "./components/PostsContainer/PostsPage"
 const App = () => {
+
   // set up state for the dummy data and pass to your PostsPage
+  
+  const [initialState] = useState(dummyData);
+  
+  // console.log(postData)
+  
+  const [postData] = useState(initialState);
+  
   return (
     <div className="App">
-      {/* Add components here  and pass props where appropriate */}
+      <h1>This is our new app</h1>
+{/* Add components here  and pass props where appropriate  */}
+      <PostsPage postData = {postData} />
+
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,10 +4,15 @@ import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+export const PostsPage = props => {
+  console.log(props)
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
+      {props.postData.map((data,index) => {
+        return <Post key={index} post={data}/>
+      })};
+
     </div>
   );
 };
